8f4f0eaeaa369c9b0f87c7432d313a47cb5ed125
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2016-05-20 19:20:19 +0200

    basic mqqt publication working

---
+++ src/publisher.c
@@ -0,0 +1,51 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "MQTTClient.h"
+
+#define ADDRESS         "tcp://m12.cloudmqtt.com:17065"
+#define CLIENTID        "ExampleClientPub"
+#define USERNAME        "camflow"
+#define PASSWORD        "test"
+#define TOPIC           "camflow/test"
+#define PAYLOAD         "Ok and now?"
+#define QOS             1
+#define TIMEOUT         10000L
+
+MQTTClient client;
+
+void publish(char* payload, int qos){
+  int rc;
+  MQTTClient_message pubmsg = MQTTClient_message_initializer;
+  MQTTClient_deliveryToken token;
+  pubmsg.payload = payload;
+  pubmsg.payloadlen = strlen(payload);
+  pubmsg.qos = qos;
+  pubmsg.retained = 0;
+  MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+}
+
+int main(int argc, char* argv[])
+{
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+
+    int rc;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+    conn_opts.username = USERNAME;
+    conn_opts.password = PASSWORD;
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
+    }
+
+    publish(PAYLOAD, QOS);
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
+}
