62b94f320eab6a8f2f31e4a17364f70103581a85
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2016-07-19 15:24:41 +0200

    pushing json data to mqtt

--- src/publisher.c
+++ src/publisher.c
@@ -8,6 +8,7 @@
 
 #include "MQTTClient.h"
 #include "provenancelib.h"
+#include "provenancePovJSON.h"
 #include "simplog.h"
 
 #define	LOG_FILE "/tmp/audit.log"
@@ -17,8 +18,6 @@
 #define CLIENTID        "ExampleClientPub"
 #define USERNAME        "camflow"
 #define PASSWORD        "test"
-#define TOPIC           "camflow/test"
-#define PAYLOAD         "Ok and now?"
 #define QOS             1
 #define TIMEOUT         10000L
 
@@ -63,65 +62,54 @@ void _init_logs( void ){
   simplog.setLogDebugLevel(SIMPLOG_VERBOSE);
 }
 
-static __thread char buffer[10192]; // check the size
-
 void init( void ){
   pid_t tid = gettid();
   simplog.writeLog(SIMPLOG_INFO, "audit writer thread, tid:%ld", tid);
 }
 
 void log_str(struct str_struct* data){
-  simplog.writeLog(SIMPLOG_INFO, str_msg_to_json(buffer, data));
-}
-
-void log_link(struct link_struct* link){
-  simplog.writeLog(SIMPLOG_INFO, link_to_json(buffer, link));
-}
-
-void log_unlink(struct unlink_struct* unlink){
-  simplog.writeLog(SIMPLOG_INFO, unlink_to_json(buffer, unlink));
+  //append_entity(str_msg_to_json(data));
 }
 
 void log_edge(struct edge_struct* edge){
-  mqqt_publish("camflow/edges", edge_to_json(buffer, edge), QOS);
+  append_edge(edge_to_json(edge));
 }
 
 void log_task(struct task_prov_struct* task){
-  mqqt_publish("camflow/activities", task_to_json(buffer, task), QOS);
+  append_activity(task_to_json(task));
 }
 
 void log_inode(struct inode_prov_struct* inode){
-  mqqt_publish("camflow/entities", inode_to_json(buffer, inode), QOS);
+  append_entity(inode_to_json(inode));
 }
 
 void log_disc(struct disc_node_struct* node){
-  // TODO allow disclosing process to set type
-  mqqt_publish("camflow/entities", disc_to_json(buffer, node), QOS);
+  append_entity(disc_to_json(node));
 }
 
 void log_msg(struct msg_msg_struct* msg){
-  mqqt_publish("camflow/entities", msg_to_json(buffer, msg), QOS);
+  append_entity(msg_to_json(msg));
 }
 
 void log_shm(struct shm_struct* shm){
-  mqqt_publish("camflow/entities", shm_to_json(buffer, shm), QOS);
+  append_entity(shm_to_json(shm));
 }
 
 
 void log_sock(struct sock_struct* sock){
-  mqqt_publish("camflow/entities", sock_to_json(buffer, sock), QOS);
+  append_entity(sock_to_json(sock));
 }
 
 void log_address(struct address_struct* address){
-  simplog.writeLog(SIMPLOG_INFO, addr_to_json(buffer, address));
+  append_entity(addr_to_json(address));
 }
 
 void log_file_name(struct file_name_struct* f_name){
-  simplog.writeLog(SIMPLOG_INFO, pathname_to_json(buffer, f_name));
+  append_entity(pathname_to_json(f_name));
 }
 
 void log_ifc(struct ifc_context_struct* ifc){
-  simplog.writeLog(SIMPLOG_INFO, ifc_to_json(buffer, ifc));
+  append_entity(ifc_to_json(ifc));
 }
 
 struct provenance_ops ops = {
@@ -130,8 +118,6 @@ struct provenance_ops ops = {
   .log_task=log_task,
   .log_inode=log_inode,
   .log_str=log_str,
-  .log_link=log_link,
-  .log_unlink=log_unlink,
   .log_disc=log_disc,
   .log_msg=log_msg,
   .log_shm=log_shm,
@@ -140,6 +126,11 @@ struct provenance_ops ops = {
   .log_file_name=log_file_name,
   .log_ifc=log_ifc
 };
+
+void print_json(char* json){
+  sleep(1); // demo use free version we don't want to go over bandwith limit
+  mqqt_publish("camflow", json, QOS);
+}
 
 int main(int argc, char* argv[])
 {
@@ -150,14 +141,13 @@ int main(int argc, char* argv[])
 
     MQTTClient_create(&client, ADDRESS, CLIENTID,
         MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    mqqt_publish(TOPIC, PAYLOAD, QOS);
 
     rc = provenance_register(&ops);
     if(rc){
       simplog.writeLog(SIMPLOG_ERROR, "Failed registering audit operation.");
       exit(rc);
     }
-    sleep(2);
+    set_ProvJSON_callback(print_json);
     while(1) sleep(60);
 
     // never reached
